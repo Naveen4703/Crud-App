38ce9ef4ce59c8a4c48572af0efbe1dc5c561bd3
Xd666 / Naveen <63446662+Naveen4703@users.noreply.github.com>
2023-07-17 08:02:13 +0530

    Update router.js

--- src/routes/router.js
+++ src/routes/router.js
@@ -4,7 +4,7 @@ const controller = require("../controllers/student.js");
 
 
 router.get('/',(req,res)=>{
-    res.status(200).json({Response : "Server Is running..."});
+    res.status(200).json({ Response : "Server Is running...  Use :  {GET} -> /api/students -- for All student data ,  {GET} -> /api/student/{student Id} -- for coresponding student Id data  {POST} -> /api/addStudent with body data {name, age, gender, studentId} {PUT} -> /api/update -- for updating student data using Query - id {DELETE} -> /api/delete -- for deleting student data using Query - id  ",   });
 });
 
 
